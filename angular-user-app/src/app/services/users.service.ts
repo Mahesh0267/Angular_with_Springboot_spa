ec80ba545ed5d07aea18f5c103df5c420eaa6c84
Mahesh0267 <106401664+Mahesh0267@users.noreply.github.com>
2022-06-21 14:47:45 +0530

    retriving list of user and showing table

--- angular-user-app/src/app/services/users.service.ts
+++ angular-user-app/src/app/services/users.service.ts
@@ -3,7 +3,6 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 const rootUrl = 'http://localhost:8090/api/user';
-const getUrl = 'http://localhost:8090/api/user/get-all-users';
 
 @Injectable({
   providedIn: 'root',
@@ -12,7 +11,7 @@ export class UsersService {
   constructor(private http: HttpClient) {}
 
   getAll(): Observable<any> {
-    return this.http.get(getUrl);
+    return this.http.get(rootUrl + '/get-all-users');
   }
   get(username: string): Observable<any> {
     return this.get(`${rootUrl}/${username}`);
