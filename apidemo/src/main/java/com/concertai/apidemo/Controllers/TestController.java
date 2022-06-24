d0e888d8385ac2f88782e65deb0a4336a8f06192
Mahesh0267 <106401664+Mahesh0267@users.noreply.github.com>
2022-06-24 17:18:11 +0530

    added admin fetch data

--- apidemo/src/main/java/com/concertai/apidemo/Controllers/TestController.java
+++ apidemo/src/main/java/com/concertai/apidemo/Controllers/TestController.java
@@ -1,15 +1,29 @@
 package com.concertai.apidemo.Controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.concertai.apidemo.Repos.UserRepository;
+import com.concertai.apidemo.models.User;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
+
+    @Autowired
+    UserRepository userRespository;
+
     @GetMapping("/all")
     public String allAccess() {
         return "Public Content.";
@@ -29,7 +43,7 @@ public class TestController {
 
     @GetMapping("/admin")
     @PreAuthorize("hasRole('ADMIN')")
-    public String adminAccess() {
-        return "Admin Board.";
+    public List<User> adminAccess() {
+        return userRespository.findAll();
     }
 }
