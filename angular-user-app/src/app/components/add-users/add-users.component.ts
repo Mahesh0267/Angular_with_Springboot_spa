31e65dbbc0c86936f0631acf071e40b26a731998
Mahesh0267 <106401664+Mahesh0267@users.noreply.github.com>
2022-06-24 22:43:23 +0530

    added login

---
+++ angular-user-app/src/app/components/add-users/add-users.component.ts
@@ -0,0 +1,68 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { UsersService } from 'src/app/services/users.service';
+import { AlertService } from '../alerts';
+import { ListUsersComponent } from '../list-users/list-users.component';
+
+interface DialogData {
+  email: string;
+}
+@Component({
+  selector: 'app-add-users',
+  templateUrl: './add-users.component.html',
+  styleUrls: ['./add-users.component.css'],
+})
+export class AddUsersComponent implements OnInit {
+  form!: FormGroup;
+  isEditable = false;
+  submitted = false;
+  users: any;
+  userId!: number;
+  constructor(
+    private usersService: UsersService,
+    private alertService: AlertService,
+    public dialogRef: MatDialogRef<AddUsersComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {}
+
+  ngOnInit(): void {
+    this.initFrom();
+    this.getUsersList();
+  }
+
+  getUsersList(): void {
+    this.usersService.getAll().subscribe(
+      (data) => {
+        this.users = data;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  private initFrom() {
+    this.form = new FormGroup({
+      username: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      role: new FormControl('', Validators.required),
+    });
+
+    if (this.isEditable) {
+      this.form.patchValue(this.data);
+    }
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.usersService.create(this.form.value).subscribe({
+        next: (res) => {
+          this.form.reset();
+          this.dialogRef.close('save');
+        },
+        error: (e) => console.error(e),
+      });
+    }
+  }
+}
---
+++ angular-user-app/src/app/components/list-users/list-users.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { AddUsersComponent } from '../add-users/add-users.component';
+import { UserDetailsComponent } from '../user-details/user-details.component';
+
+@Component({
+  selector: 'app-list-users',
+  templateUrl: './list-users.component.html',
+  styleUrls: ['./list-users.component.css'],
+})
+export class ListUsersComponent implements OnInit {
+  users: any;
+  username = '';
+  userId!: number;
+  email: string = '';
+
+  constructor(private usersService: UsersService, public dialog: MatDialog) {}
+
+  ngOnInit(): void {
+    this.getUsersList();
+  }
+
+  getUsersList(): void {
+    this.usersService.getAll().subscribe(
+      (data) => {
+        this.users = data;
+        console.log(data);
+        console.log(typeof data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+  searchUser(): void {
+    this.usersService.get().subscribe(
+      (data) => {
+        this.users = data;
+        console.log(data);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog
+      .open(AddUsersComponent, {
+        width: '650px',
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'save') {
+          this.getUsersList();
+        }
+      });
+  }
+
+  deleteUser(id: number): void {
+    this.usersService.delete(id).subscribe(() => {
+      console.log('deleted');
+      this.getUsersList();
+    });
+  }
+  editDialog(id: number): void {
+    this.userId = id;
+    const dialogRef = this.dialog
+      .open(UserDetailsComponent, {
+        width: '650px',
+        data: id,
+      })
+      .afterClosed()
+      .subscribe((val) => {
+        if (val === 'update') {
+          this.getUsersList();
+        }
+        console.log('Dialog closed');
+      });
+  }
+}
